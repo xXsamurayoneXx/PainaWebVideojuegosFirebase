b80ed5f495caa3635bbb8f2bdef7220c5b94a352
xXsamurayoneXx <55166601+xXsamurayoneXx@users.noreply.github.com>
2022-12-06 17:24:41 -0600

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('.menu').click(function() {
+        $('.overlay').toggleClass('anim');
+        $(this).addClass('open')
+    });
+
+    $('.open').click(function(){
+        $('.overlay').toggleClass('reverse-animation');
+    })
+});
